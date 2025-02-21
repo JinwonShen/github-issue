5ceebd41c96473d570f26f825b74558587b0dae5
JinwonShen <bosv031999@gmail.com>
2025-02-22 02:19:36 +0900

    github-issue fin

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { useQuery } from "@tanstack/react-query"
 import { GITHUB_API } from "./api"
 import axios from "axios"
